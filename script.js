43c5a1ff0e2d33237b1e3e2bb3c306c59b79fc48
Jake <jakethepiglet@gmail.com>
2024-07-15 20:25:43 -0500

    Add grid and coloring

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const container = document.querySelector("#container");
+let divNum = 16;
 
-function makeDivs(divNum) {
-    for (let i=0; i<divNum; i++) {
+function makeDivs() {
+    for (let i=1; i<=divNum**2; i++) {
         const square = document.createElement("div");
-        square.id = "s"+i;
-        square.className = "square";
+        square.classList.add("square");
+        square.classList.add(`square-${i}`);
+        container.appendChild(square);
+        square.setAttribute("style", `height: ${(1000-(2*divNum))/divNum}px; 
+        width: ${(1000-(2*divNum))/divNum}px;`)
     }
 }
 
-makeDivs(256);
-container.appendChild(square);
+function hoveringColor() {
+    let squares = document.getElementsByClassName("square");
+    for(let i=0; i<squares.length; i++) {
+        squares[i].addEventListener("mousedown", () => isDrawing = true);
+        squares[i].addEventListener("mouseup", () => isDrawing = false);
+        squares[i].addEventListener("mousemove", function() {
+            if (isDrawing) {
+                this.style.backgroundColor = "black";
+            }
+        });
+    }
+}
+
+makeDivs();
+hoveringColor();
